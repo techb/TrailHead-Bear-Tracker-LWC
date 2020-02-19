39bc01ea9e08404b0b946c6d9d6a0d9c5e630427
K.B. Carte <kbcarte@gmail.com>
2020-02-19 16:21:39 -0500

    added new tile component with styles

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -2,14 +2,15 @@ import { LightningElement, wire, track } from 'lwc';
 import ursusResources from '@salesforce/resourceUrl/ursus_park';
 // BearController.getAllBears() Apex method
 import searchBears from '@salesforce/apex/BearController.searchBears';
+import { loadStyle } from 'lightning/platformResourceLoader';
 
 export default class BearList extends LightningElement {
     @track searchTerm = '';
     @wire(searchBears, {searchTerm: '$searchTerm'})
     bears;
-    appResources = {
-        bearSilhouette: ursusResources+'/img/standing-bear-silhouette.png',
-    };
+    connectedCallback() {
+        loadStyle(this, ursusResources + '/style.css');
+    }
     handlSearchTermChange(event){
         // Debouncing this method: do not update the reactive property as
         // long as this function is being called within a delay of 300 ms.
