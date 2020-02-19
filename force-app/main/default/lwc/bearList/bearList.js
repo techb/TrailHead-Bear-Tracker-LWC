9dfdbe31185a8ee1a454dac8e10e945912d646ed
K.B. Carte <kbcarte@gmail.com>
2020-02-19 16:26:22 -0500

    added functionality to each tile, now on click they redirect to respective pages

--- force-app/main/default/lwc/bearList/bearList.js
+++ force-app/main/default/lwc/bearList/bearList.js
@@ -1,27 +1,41 @@
-import { LightningElement, wire, track } from 'lwc';
-import ursusResources from '@salesforce/resourceUrl/ursus_park';
-// BearController.getAllBears() Apex method
-import searchBears from '@salesforce/apex/BearController.searchBears';
+import { NavigationMixin } from 'lightning/navigation';
 import { loadStyle } from 'lightning/platformResourceLoader';
-
-export default class BearList extends LightningElement {
-    @track searchTerm = '';
-    @wire(searchBears, {searchTerm: '$searchTerm'})
-    bears;
-    connectedCallback() {
-        loadStyle(this, ursusResources + '/style.css');
-    }
-    handlSearchTermChange(event){
-        // Debouncing this method: do not update the reactive property as
-        // long as this function is being called within a delay of 300 ms.
-        // This is to avoid a very large number of Apex method calls
-        window.clearTimeout(this.delayTimeout);
-        const searchTerm = event.target.value;
-        this.delayTimeout = setTimeout(() => {
-            this.searchTerm = searchTerm;
-        }, 300);
-    }
-    get hasResults(){
-        return (this.bears.data.length > 0);
-    }
+import ursusResources from '@salesforce/resourceUrl/ursus_park';
+import { LightningElement, track, wire } from 'lwc';
+/** BearController.searchBears(searchTerm) Apex method */
+import searchBears from '@salesforce/apex/BearController.searchBears';
+export default class BearListNav extends NavigationMixin(LightningElement) {
+	@track searchTerm = '';
+	@wire(searchBears, {searchTerm: '$searchTerm'})
+	bears;
+	connectedCallback() {
+		loadStyle(this, ursusResources + '/style.css');
+	}
+	handleSearchTermChange(event) {
+		// Debouncing this method: do not update the reactive property as
+		// long as this function is being called within a delay of 300 ms.
+		// This is to avoid a very large number of Apex method calls.
+		window.clearTimeout(this.delayTimeout);
+		const searchTerm = event.target.value;
+		// eslint-disable-next-line @lwc/lwc/no-async-operation
+		this.delayTimeout = setTimeout(() => {
+			this.searchTerm = searchTerm;
+		}, 300);
+	}
+	get hasResults() {
+		return (this.bears.data.length > 0);
+	}
+	handleBearView(event) {
+		// Get bear record id from bearview event
+		const bearId = event.detail;
+		// Navigate to bear record page
+		this[NavigationMixin.Navigate]({
+			type: 'standard__recordPage',
+			attributes: {
+				recordId: bearId,
+				objectApiName: 'Bear__c',
+				actionName: 'view',
+			},
+		});
+	}
 }
--- force-app/main/default/lwc/bearTile/bearTile.js
+++ force-app/main/default/lwc/bearTile/bearTile.js
@@ -4,5 +4,12 @@ export default class BearTile extends LightningElement {
 	@api bear;
 	appResources = {
 		bearSilhouette: ursusResources +'/img/standing-bear-silhouette.png',
-	};
+    };
+
+    handleOpenRecordClick() {
+        const selectEvent = new CustomEvent('bearview', {
+            detail: this.bear.Id
+        });
+        this.dispatchEvent(selectEvent);
+    }
 }
